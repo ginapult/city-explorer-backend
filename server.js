8e46c1b5b467dc4670cc900e1110dc8b9007d5a7
Gina Pultorak <gina.pultorak@gmail.com>
2020-01-14 10:37:47 -0800

    server up on port

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Port ${PORT} is listening!`)
+});
